3ba5183db587184a3577909f90695fdad183f11a
Joaquim DaPonte <xhantorz@Joaquims-MacBook-Pro.local>
2017-10-30 16:55:28 -0300

    Mac

--- server.js
+++ server.js
@@ -1,28 +1,22 @@
 // require all dependencies
 var express = require('express');
 var logger = require('morgan');
+var path = require('path');
 var app = express();
+var bodyParser = require("body-parser");
 
 // add logging and static middleware to express
 app.use(logger('dev'));
 
 // set up the template engine
 app.set('views', './views');
-app.set('css', './css');
 app.set('view engine', 'pug');
+
+app.use(express.static(path.join(__dirname, './public')));
 
 // GET response for '/'
 app.get('/', function (req, res, next) {
-
-    try {
-
-        // render the 'index' template, and pass in a few variables
-        res.render('index', { title: 'Hey', message: 'Hello there!' });
-
-    } catch (e) { // if there are any errors, send them off the the logger
-        next(e);
-    }
-
+  res.render('index');
 });
 
 // start up the server
