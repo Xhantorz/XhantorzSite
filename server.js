3e2384837f82b8753956265dd255354e429a402b
Joaquim DaPonte <xhantorz@Joaquims-MacBook-Pro.local>
2017-10-30 13:21:00 -0300

    Routers

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ var app = express();
 var port = 8080;
 
 var router = require('./app/routes');
+
+app.use(express.static(__dirname + '/public'));
 app.use('/', router);
 
 app.listen(port, function(){
